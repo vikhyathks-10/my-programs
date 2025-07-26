7ffac8b63a42a7c9257bd25a8a431ff91c86ad96
Vikhyath KS <vikhyathks2022@gmail.com>
2025-07-26 12:38:25 +0530

    adding 3 numbers

---
+++ add3.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+sum = num1 + num2 + num3
+
+print(f"The sum of {num1}, {num2}, and {num3} is {sum}")
