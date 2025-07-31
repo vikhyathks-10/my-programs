4b6c1bcc42051badd36ce90f07ab719f14686b47
Vikhyath KS <vikhyathks2022@gmail.com>
2025-07-31 20:19:14 +0530

    Renamed wheather-check.py to weather_check.py

--- phonenumber-check.py
+++ phonenumber-check.py
@@ -1,6 +1,6 @@
 phone = input("Enter phone number: ")
 
 if len(phone) == 10 and phone.isdigit():
-    print("Valid phone number.")
+    print(" Valid phone number.")
 else:
     print("Invalid input! Please enter a 10-digit phone number consisting of digits only.")
