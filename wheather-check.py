2e542ee3ee2d0ab3460a591006d69236c4854b19
Vikhyath KS <vikhyathks2022@gmail.com>
2025-08-01 11:53:08 +0530

    adding a next 5 programs

--- wheather-check.py
+++
@@ -1,8 +0,0 @@
-temp = float(input("Enter current temperature (°C): "))
-
-if temp < 20:
-    print("Wear warm clothes.")
-elif 20 <= temp <= 30:
-    print("Comfortable weather.")
-else:
-    print("Stay hydrated.")
