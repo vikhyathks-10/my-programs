20bb167071f1d5a5e196aeb399bfcf6e9966d877
Vikhyath KS <vikhyathks2022@gmail.com>
2026-01-04 09:39:53 +0530

    Day 3: Basic arithmetic operations in Python

---
+++ day3.py
@@ -0,0 +1,25 @@
+# DAY 3 – Basic Arithmetic Operations
+
+# Taking two numbers from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Addition
+addition = a + b
+print("\nAddition:", addition)
+
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+
+# Division
+division = a / b
+print("Division:", division)
+
+# Modulus
+modulus = a % b
+print("Modulus:", modulus)
