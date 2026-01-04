84233c955dd7e206624cdb5148dba18681dfccf0
Vikhyath KS <vikhyathks2022@gmail.com>
2026-01-04 09:28:06 +0530

    Day 1: Basic input, output, and variables in Python

---
+++ day1a.py
@@ -0,0 +1,27 @@
+# DAY 1 – Input, Output, Variables
+
+# 1. Print "Hello, World"
+print("Hello, World")
+
+# 2. Print your name
+name = "Vikyat"
+print("Name:", name)
+
+# 3. Print name, age, college
+age = 19
+college = "PES College, Bangalore"
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+
+# 4. Add two numbers (fixed values)
+a = 10
+b = 20
+sum_fixed = a + b
+print("Sum of fixed numbers:", sum_fixed)
+
+# 5. Add two numbers (user input)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum_user = num1 + num2
+print("Sum of user input numbers:", sum_user)
