f24f9f68d7bc511e703f239c8273b59bb7e1d76e
Vikhyath KS <vikhyathks2022@gmail.com>
2026-01-04 09:35:03 +0530

    Day 2: Data types and type conversion in Python

---
+++ day2.py
@@ -0,0 +1,30 @@
+# DAY 2 – Data Types
+
+# 1. Check type of int
+a = 10
+print("Value:", a)
+print("Type of a:", type(a))
+
+# 2. Check type of float
+b = 10.5
+print("\nValue:", b)
+print("Type of b:", type(b))
+
+# 3. Check type of string
+c = "Python"
+print("\nValue:", c)
+print("Type of c:", type(c))
+
+# 4. Convert int to float
+num_int = 25
+num_float = float(num_int)
+print("\nInteger value:", num_int)
+print("Converted to float:", num_float)
+print("Type after conversion:", type(num_float))
+
+# 5. Convert string to int
+num_str = "100"
+num_int2 = int(num_str)
+print("\nString value:", num_str)
+print("Converted to int:", num_int2)
+print("Type after conversion:", type(num_int2))
