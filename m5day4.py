500de27e273cecccf5645b6f2a3460f6222c4798
vikhyath bharadwaj k s <vikhyathks2022@gmail.com>
2026-05-04 09:16:05 +0530

    Implemented time module programs including digital clock, stopwatch, sleep delay, execution time measurement, and countdown timer(modified)

--- m5day4.py
+++ m5day4.py
@@ -1,81 +1,65 @@
-# 🔹 DAY 25 - STRING PROBLEMS
+# 🔹 DAY 4 - TIME MODULE
+
+import time
 
 
-# 🔹 1. Anagram Check
-def is_anagram(s1, s2):
-    return sorted(s1) == sorted(s2)
+# 🔹 1. Digital Clock
+
+print("\n--- Digital Clock ---")
+
+for i in range(5):   # runs 5 times
+    current_time = time.strftime("%H:%M:%S")
+    print("Current Time:", current_time)
+    time.sleep(1)
 
 
-# 🔹 2. Character Frequency
-def char_frequency(s):
-    freq = {}
-    for ch in s:
-        freq[ch] = freq.get(ch, 0) + 1
-    return freq
+# 🔹 2. Stopwatch
+
+print("\n--- Stopwatch ---")
+
+start = time.time()
+
+time.sleep(2)   # simulate task
+
+end = time.time()
+
+print("Elapsed Time:", round(end - start, 2), "seconds")
 
 
-# 🔹 3. Longest Substring Without Repeating Characters
-def longest_substring(s):
-    char_set = set()
-    left = 0
-    max_len = 0
+# 🔹 3. Delay Execution using sleep()
 
-    for right in range(len(s)):
-        while s[right] in char_set:
-            char_set.remove(s[left])
-            left += 1
+print("\n--- Sleep Example ---")
 
-        char_set.add(s[right])
-        max_len = max(max_len, right - left + 1)
-
-    return max_len
+print("Program starts...")
+time.sleep(3)
+print("Program resumed after 3 seconds")
 
 
-# 🔹 4. Palindrome Substring (Check existence)
-def has_palindrome_substring(s):
-    n = len(s)
+# 🔹 4. Measure Execution Time
 
-    for i in range(n):
-        for j in range(i+1, n+1):
-            sub = s[i:j]
-            if sub == sub[::-1] and len(sub) > 1:
-                return True
-    return False
+print("\n--- Execution Time ---")
 
+start_time = time.time()
 
-# 🔹 5. String Compression
-def compress_string(s):
-    result = ""
-    count = 1
+total = 0
+for i in range(1000000):
+    total += i
 
-    for i in range(1, len(s)):
-        if s[i] == s[i-1]:
-            count += 1
-        else:
-            result += s[i-1] + str(count)
-            count = 1
+end_time = time.time()
 
-    result += s[-1] + str(count)
-    return result
+print("Execution Time:",
+      round(end_time - start_time, 4), "seconds")
 
 
-# 🔹 MAIN PROGRAM
+# 🔹 5. Countdown Timer
 
-print("\n--- Anagram ---")
-print(is_anagram("listen", "silent"))
+print("\n--- Countdown Timer ---")
 
+seconds = 5
 
-print("\n--- Character Frequency ---")
-print(char_frequency("programming"))
+while seconds > 0:
+    print("Time Left:", seconds)
+    time.sleep(1)
+    seconds -= 1
 
-
-print("\n--- Longest Substring ---")
-print(longest_substring("abcabcbb"))
-
-
-print("\n--- Palindrome Substring ---")
-print(has_palindrome_substring("babad"))
-
-
-print("\n--- String Compression ---")
-print(compress_string("aaabbc"))
+print("Time's Up!")
